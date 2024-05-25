d10b016c2dd1b1f70039ffbf1ae559289db392e1
XRG2014 <138714694+XRG2014@users.noreply.github.com>
2024-05-25 06:36:57 -0700

    Update node.py

--- .nvm/bin/node.py
+++ .nvm/bin/node.py
@@ -13,36 +13,36 @@ ver = "2.x"
 n = len(sys.argv)
 
 def ht():
-    print("usage: node [--help] [--version] [--in-window] [--silent] [-hv?] [file]")
-    print("-?, -h, --help:  display help text.")
-    print("-v, --version:   display current version.")
-    print("--in-window:     runs inside the main window (can change terminal appearance or behaviour; use with caution).")
-    print("--silent:        do not print the result of the JavaScript execution.")
+    print("usage: node [--help] [--version] [--in-window] [--silent] [-hv?] <file>")
+    print("-?, -h, --help:  display help text")
+    print("-v, --version:   display current version")
+    print("--in-window:     runs inside the main window (this can change terminal appearance or behaviour; use with caution)")
+    print("--silent:        do not print output of the JavaScript execution")
 
 try:
     if sys.argv[1] == "-?" or sys.argv[1] == "-h" or sys.argv[1] == "--help":
         if n >= 3:
-            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} too many arguments.')
+            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} too many arguments')
             ht()
             sys.exit()
         ht()
         sys.exit()
     elif sys.argv[1] == "-v" or sys.argv[1] == "--version":
         if n >= 3:
-            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} too many arguments.')
+            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} too many arguments')
             ht()
             sys.exit()
         print(f'{ver} / LTS')
     elif sys.argv[1] == "--in-window":
         if not n >= 3:
-            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} not enough arguments.')
+            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} not enough arguments')
             ht()
             sys.exit()
         for i in range(2, n):
             os.system(f'jsc {sys.argv[i]} --in-window')
     elif sys.argv[1] == "--silent":
         if not n >= 3:
-            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} not enough arguments.')
+            print(f'{Fore.LIGHTRED_EX}Error:{Fore.RESET} not enough arguments')
             ht()
             sys.exit()
         for i in range(2, n):
