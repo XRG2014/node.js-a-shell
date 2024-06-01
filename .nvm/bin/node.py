e7456a6570fb9d9187b4c5c2dfe961040932af04
XRG2014 <138714694+XRG2014@users.noreply.github.com>
2024-06-01 06:03:16 -0700

    Update node.py

--- .nvm/bin/node.py
+++ .nvm/bin/node.py
@@ -1,13 +1,7 @@
 import sys
 import os
 import time
-try:
-    from colorama import Fore
-except ImportError:
-    print("Installing Dependencies...")
-    time.sleep(0.9)
-    pkgs = "colorama"
-    os.system(f'python3 -ubm pip install {pkgs}')
+
 ver = "2.x"
 
 n = len(sys.argv)
